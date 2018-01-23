92c816efea0cac2b625717c59b8ea3e1006cb4a3
davchoo <davchoo3@gmail.com>
2018-01-22 18:57:20 -0800

    Arm Stub

--- src/main/java/frc/team6718/robot/RobotMap.java
+++ src/main/java/frc/team6718/robot/RobotMap.java
@@ -13,4 +13,11 @@ public class RobotMap {
     public static final int DRIVE_TRAIN_ENCODER_LEFT_B = 1;
     public static final int DRIVE_TRAIN_ENCODER_RIGHT_A = 2;
     public static final int DRIVE_TRAIN_ENCODER_RIGHT_B = 3;
+
+    //Arm potentiometers (AI pins)
+    public static final int ARM_POTENTIOMETER_JOINT_0 = 0;
+    public static final int ARM_POTENTIOMETER_JOINT_1 = 1;
+    public static final int ARM_POTENTIOMETER_JOINT_2 = 2;
+
+    
 }
